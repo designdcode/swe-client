1160b6d3f0d29703863c19cb44b276cc868653c2
Jin Park <jin.park@kidways.fr>
2022-04-25 22:58:14 +0200

    up

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -15,7 +15,7 @@ const cache: InMemoryCache = new InMemoryCache();
 
 const httpLink = new HttpLink({
   // uri: "http://swedu.sunmoon.ac.kr/graphql"
-  uri: "http://localhost:4000/graphql",
+  uri: "http://localhost:4000",
   // uri: "http://ec2-15-165-19-80.ap-northeast-2.compute.amazonaws.com/graphql",
 });
 
