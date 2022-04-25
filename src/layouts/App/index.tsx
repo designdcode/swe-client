aaad096ffd9fd80748ba4924e8cd41d9424eac07
Jin Park <jin.park@kidways.fr>
2022-04-25 23:07:09 +0200

    up

--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -11,13 +11,13 @@ const Renewal = lodable(() => import("../Renewal"))
 const App: React.FC = () => {
   return (
     <Switch>
-      <Redirect exact path="/" to="/main" />
       {/* <Redirect exact path="/" to="/renewal" /> */}
+      <Redirect exact path="/" to="/main" />
       <Redirect exact path="/admin" to="/admin/dashboard/dashboard" />
 
-      <Route path="/renewal" component={Renewal} />
       <Route path="/main" component={Main} />
       <Route path="/admin/:param/:subparam" component={Admin} />
+      <Route path="/renewal" component={Renewal} />
     </Switch>
   );
 };
