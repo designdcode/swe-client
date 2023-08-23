78c5b33661845b5e62614d6523ed97e1aee22e7c
JinPark <jinchul112@gmail.com>
2023-08-23 15:15:37 +0200

    wip: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,40 +1,64 @@
 import styled from "@emotion/styled";
 import {
-  breakpoints,
   BREAKPOINT_BIGGER_THAN_PC,
   BREAKPOINT_PHONE_MEDIUM,
   mediaQueries,
 } from "../../utils/mediaQuery";
+import { Divider, Image, Typography } from "antd";
 
 const Footer = () => {
   return (
     <FooterContaier>
-      <img src={"/img/footerLogo.jpg"} alt="logoimg" className="mobile" />
-      <div className="image-link">
-        <a
-          href="https://www.instagram.com/sw_sunmoonuniv/"
-          target="_blank"
-          rel="noreferrer"
+      <Div>
+        <div className="banner-group">
+          <Image src={"/img/footerBanner2.jpg"} />
+          <Image src={"/img/footerBanner1.jpg"} />
+        </div>
+        <img src={"/img/footerLogo.jpg"} alt="logoimg" className="logo" />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          <img src={"/img/insta.png"} alt="insta" />
-        </a>
-        <a
-          href="https://www.youtube.com/channel/UC6CYp3QV8cL-D4-hBXb_Nbw"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={"/img/youtube.png"} alt="youtube" />
-        </a>
-      </div>
-      <span>
-        TEL . 041-530-8311 FAX. 041-530-8315 <br />
-        우) 31460 충남 아산시 탕정면 선문로 221번길 70 선문대학교
-        SW융합관(원화관) 506호 70,
-        <br />
-        Sunmoon-ro 221 beon-gil, Tangjeong-myeon, Asan-si, Chungcheongnam-do,
-        korea , 31460
-      </span>
-      <img src={"/img/footerLogo.jpg"} alt="logoimg" className="web" />
+          <Typography.Text style={{ color: "#BFBFBF" }}>
+            {" "}
+            TEL . 041-530-8311 FAX. 041-530-8315
+          </Typography.Text>
+          <Typography.Text style={{ color: "#BFBFBF" }}>
+            우) 31460 충남 아산시 탕정면 선문로 221번길 70 선문대학교
+            SW융합관(원화관) 506호 70
+          </Typography.Text>
+          <Typography.Text style={{ color: "#BFBFBF" }}>
+            Sunmoon-ro 221 beon-gil, Tangjeong-myeon, Asan-si,
+            Chungcheongnam-do, korea , 31460
+          </Typography.Text>
+        </div>
+        <Divider
+          style={{
+            backgroundColor: "#BFBFBF",
+          }}
+        />
+        <div className="image-link">
+          <a
+            href="https://www.instagram.com/sw_sunmoonuniv/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={"/img/insta.png"} alt="insta" />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UC6CYp3QV8cL-D4-hBXb_Nbw"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={"/img/youtube.png"} alt="youtube" />
+          </a>
+        </div>
+      </Div>
+      <MobileDiv>mobile</MobileDiv>
     </FooterContaier>
   );
 };
@@ -42,71 +66,44 @@ const Footer = () => {
 export default Footer;
 export const FooterContaier = styled.div`
   width: 100%;
+  background-image: url("/img/footerBG.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
   ${mediaQueries(BREAKPOINT_PHONE_MEDIUM)} {
-    height: 150px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 0 20px;
-    background-color: #ededed;
-    font-size: 8px;
-    span {
-      display: block;
-      max-width: ${breakpoints.phoneMedium}px;
-      color: #747474;
-      line-height: 1.7;
-    }
-
-    & .mobile {
-      width: 150px;
-      object-fit: contain;
-      margin-bottom: 20px;
-    }
-
-    & .web {
-      display: none;
-    }
-    & .image-link {
-      a {
-        img {
-          width: 15px;
-          margin: 0 10px;
-        }
-      }
-    }
   }
   ${mediaQueries(BREAKPOINT_BIGGER_THAN_PC)} {
-    height: 230px;
+    width: 100%;
+  }
+`;
+
+const Div = styled.div`
+  ${mediaQueries(BREAKPOINT_PHONE_MEDIUM)} {
+    display: none;
+  }
+  ${mediaQueries(BREAKPOINT_BIGGER_THAN_PC)} {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background-color: #ededed;
-    min-width: 1280px;
-    -webkit-box-shadow: inset 0px 12px 30px 0px rgba(0, 0, 0, 0.28);
-    box-shadow: inset 0px 10px 15px -10px rgba(0, 0, 0, 0.28);
-    span {
-      font-size: 14px;
-      display: block;
-      color: #aaaaaa;
-      text-align: center;
-      line-height: 1.7;
-    }
-    & .mobile {
-      display: none;
-    }
-    & .web {
-      width: 250px;
-      object-fit: contain;
-      margin-top: 20px;
-    }
-    & .image-link {
-      a {
-        img {
-          width: 20px;
-          margin: 0 10px;
-        }
+    width: 100%;
+    & .banner-group {
+      display: flex;
+      width: 60%;
+      justify-content: space-around;
+      img {
+        width: 400px;
       }
     }
+    & .logo {
+      margin: 10px;
+    }
+  }
+`;
+
+const MobileDiv = styled.div`
+  ${mediaQueries(BREAKPOINT_PHONE_MEDIUM)} {
+  }
+  ${mediaQueries(BREAKPOINT_BIGGER_THAN_PC)} {
+    display: none;
   }
 `;
